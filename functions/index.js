2c18cde5b9850c9aebf54330dc5dc9e132d5b47e
chdaniel94 <109545577+chdaniel94@users.noreply.github.com>
2024-07-10 11:54:31 -0600

    Added functions firebasecloud

---
+++ functions/index.js
@@ -0,0 +1,54 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+exports.checkEmail = functions.https.onRequest(async (req, res) => {
+  if (req.method !== "POST") {
+    return res.status(405).send("Method not allowed");
+  }
+
+  const email = req.body.email;
+
+  try {
+    await admin.auth().getUserByEmail(email);
+    return res.status(200).send({exists: true});
+  } catch (error) {
+    if (error.code === "auth/user-not-found") {
+      return res.status(200).send({exists: false});
+    } else {
+      return res.status(500).send({
+        error: "internal",
+        message: "Error checking email",
+        details: error.message,
+      });
+    }
+  }
+});
+
+exports.deleteUser = functions.https.onRequest(async (req, res) => {
+  if (req.method !== "DELETE") {
+    return res.status(405).send("Method not allowed");
+  }
+
+  const {uid} = req.body;
+
+  if (!uid) {
+    return res.status(400).send({
+      error: "bad-request",
+      message: "The UID is required",
+    });
+  }
+
+  try {
+    await admin.auth().deleteUser(uid);
+    return res.status(200).send({
+      message: "User deleted successfully",
+    });
+  } catch (error) {
+    return res.status(500).send({
+      error: "internal",
+      message: "Error deleting the user",
+      details: error.message,
+    });
+  }
+});
