cd5d232e34dd927383a36087b1aa582d4e713fcc
aLopez16 <lopezarmandospot161210@gmail.com>
2024-11-04 12:37:40 -0600

    business_type added

--- functions/index.js
+++ functions/index.js
@@ -942,6 +942,7 @@ exports.updateAccountVerification = functions.https.onRequest(async (req, res) =
   try {
     const updatedAccount = await stripe.accounts.update(accountId, {
       individual: {
+        business_type: userData.businessType,
         first_name: userData.firstName,
         last_name: userData.lastName,
         dob: {
