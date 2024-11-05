7f71f7715c4ff842b8d962847db64711d1dd13da
aLopez16 <lopezarmandospot161210@gmail.com>
2024-11-05 15:44:12 -0600

    added payout backend

--- functions/index.js
+++ functions/index.js
@@ -903,12 +903,11 @@ exports.createCustomAccount = functions.https.onCall(async (data, context) => {
         card_payments: {requested: true},
       },
     });
-    return {accountId: account.id};
+    return { accountId: account.id };
   } catch (error) {
     throw new functions.https.HttpsError("internal", error.message);
   }
 });
-
 
 exports.updateCustomAccount = functions.https.onCall(async (data, context) => {
   try {
@@ -916,7 +915,7 @@ exports.updateCustomAccount = functions.https.onCall(async (data, context) => {
       individual: {
         first_name: data.firstName,
         last_name: data.lastName,
-        dob: {day: data.day, month: data.month, year: data.year},
+        dob: { day: data.day, month: data.month, year: data.year },
         address: {
           line1: data.line1,
           postal_code: data.postalCode,
@@ -926,20 +925,7 @@ exports.updateCustomAccount = functions.https.onCall(async (data, context) => {
         },
       },
     });
-    return {success: true};
-  } catch (error) {
-    throw new functions.https.HttpsError("internal", error.message);
-  }
-});
-
-exports.createTransfer = functions.https.onCall(async (data, context) => {
-  try {
-    const transfer = await stripe.transfers.create({
-      amount: data.amount,
-      currency: "usd",
-      destination: data.accountId,
-    });
-    return {transferId: transfer.id};
+    return { success: true };
   } catch (error) {
     throw new functions.https.HttpsError("internal", error.message);
   }
@@ -950,13 +936,13 @@ exports.addCard = functions.https.onCall(async (data, context) => {
     const card = await stripe.accounts.createExternalAccount(data.accountId, {
       external_account: {
         object: "card",
-        number: data.number,
-        exp_month: data.exp_month,
-        exp_year: data.exp_year,
-        cvc: data.cvc,
+        number: "4242424242424242",
+        exp_month: 12,
+        exp_year: 2028,
+        cvc: "123",
       },
     });
-    return {success: true, cardId: card.id};
+    return { success: true, cardId: card.id };
   } catch (error) {
     throw new functions.https.HttpsError("internal", error.message);
   }
@@ -970,7 +956,34 @@ exports.acceptTos = functions.https.onCall(async (data, context) => {
         ip: context.rawRequest.ip,
       },
     });
-    return {success: true};
+    return { success: true };
+  } catch (error) {
+    throw new functions.https.HttpsError("internal", error.message);
+  }
+});
+
+exports.createTransfer = functions.https.onCall(async (data, context) => {
+  try {
+    const transfer = await stripe.transfers.create({
+      amount: data.amount,
+      currency: "mxn",
+      destination: data.accountId,
+    });
+    return { transferId: transfer.id };
+  } catch (error) {
+    throw new functions.https.HttpsError("internal", error.message);
+  }
+});
+
+exports.createPayout = functions.https.onCall(async (data, context) => {
+  try {
+    const payout = await stripe.payouts.create({
+      amount: data.amount,
+      currency: "mxn",
+    }, {
+      stripeAccount: data.accountId,
+    });
+    return { payoutId: payout.id };
   } catch (error) {
     throw new functions.https.HttpsError("internal", error.message);
   }
